6975909a12b48e9267f6f506fd32e7b7f7b59ec2
Elhadji180 <dioe20@uqo.ca>
2023-10-27 14:13:46 -0400

    premier commit du projet test. affichage du message console "hello world".

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Elhadji Thierno
+ *
+ */
+module testWithGithub {
+}
---
+++ src/testWithGithub/newProject.java
@@ -0,0 +1,8 @@
+package testWithGithub;
+
+public class newProject {
+	public static void main(String[]args) {
+		System.out.println("hello world");
+	}
+
+}
